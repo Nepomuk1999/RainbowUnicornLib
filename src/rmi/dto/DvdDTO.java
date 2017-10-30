b8d1f2972fded7b29145d8afdc8fecf3b4e203e1
dhu92 <dhutter92@gmail.com>
2017-10-30 19:28:09 +0100

    DataTransferObjects

---
+++ src/rmi/dto/DvdDTO.java
@@ -0,0 +1,44 @@
+package rmi.dto;
+
+/**
+ * Created by David on 10/30/2017.
+ */
+public class DvdDTO {
+
+    private int _dvdId;
+    private String _title;
+    private String _regisseur;
+    private String _pictureURL;
+
+    public DvdDTO(){
+
+    }
+
+    public void setDvdId(int dvdId){
+        _dvdId = dvdId;
+    }
+
+    public int getDvdId(){
+        return _dvdId;
+    }
+
+    public void setTitle(String title){
+        _title = title;
+    }
+
+    public String getTitle(){
+        return _title;
+    }
+
+    public void setRegisseur(String regisseur){
+        _regisseur = regisseur;
+    }
+
+    public String getRegisseur(){
+        return _regisseur;
+    }
+
+    public void setPictureURL(String pictureURL){
+        _pictureURL = pictureURL;
+    }
+}
