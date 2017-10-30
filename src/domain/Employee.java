d12c95603a5d0e29a9e0d22438136ca8a76b5a30
dhu92 <dhutter92@gmail.com>
2017-10-30 20:10:11 +0100

    Employee Domain Object + EmployeeDTO

---
+++ src/domain/Employee.java
@@ -0,0 +1,57 @@
+package domain;
+
+/**
+ * Created by David on 10/30/2017.
+ */
+public class Employee {
+
+    private int _employeeId;
+    private String _firstName;
+    private String _lastName;
+    private String _role;
+    private String _userName;
+
+    public Employee(){
+
+    }
+
+    public void setEmployeeId(int id){
+        _employeeId = id;
+    }
+
+    public int getEmployeeId(){
+        return _employeeId;
+    }
+
+    public void setFirstName(String firstname){
+        _firstName = firstname;
+    }
+
+    public String getFirstName(){
+        return _firstName;
+    }
+
+    public void setLastName(String lastname){
+        _lastName = lastname;
+    }
+
+    public String getLastName(){
+        return _lastName;
+    }
+
+    public void setRole(String role){
+        _role = role;
+    }
+
+    public String getRole(){
+        return _role;
+    }
+
+    public void setUsername(String username){
+        _userName = username;
+    }
+
+    public String getUsername(){
+        return _userName;
+    }
+}
