7bf1d6b030f12fa79bb44129cb885b1413175b0d
dhu92 <dhutter92@gmail.com>
2017-10-30 19:56:58 +0100

    BorrowedItem DTO und CustomerDTO + changes Customer Domain Object

---
+++ src/rmi/dto/BorrowedItemDTO.java
@@ -0,0 +1,63 @@
+package rmi.dto;
+
+import domain.Customer;
+import domain.ExternalLib;
+import domain.Media;
+
+import java.util.Date;
+
+/**
+ * Created by David on 10/30/2017.
+ */
+public class BorrowedItemDTO {
+
+    private int _borrowedId;
+    private Date _borrowedDate;
+    private ExternalLib _externalLib;
+    private Customer _customer;
+    private Media _media;
+
+    public BorrowedItemDTO(){
+
+    }
+
+    public void setBorrowedId(int id){
+        _borrowedId = id;
+    }
+
+    public int getBorrowedId(){
+        return _borrowedId;
+    }
+
+    public void setBorrowedDate(Date borrowedDate){
+        _borrowedDate = borrowedDate;
+    }
+
+    public Date getBorrowedDate(){
+        return _borrowedDate;
+    }
+
+    public void setExternalLib(ExternalLib lib){
+        _externalLib = lib;
+    }
+
+    public ExternalLib getExternalLib(){
+        return _externalLib;
+    }
+
+    public void setCustomer(Customer customer){
+        _customer = customer;
+    }
+
+    public Customer getCustomer(){
+        return _customer;
+    }
+
+    public void setMedia(Media m){
+        _media = _media;
+    }
+
+    public Media getMedia(){
+        return _media;
+    }
+}
