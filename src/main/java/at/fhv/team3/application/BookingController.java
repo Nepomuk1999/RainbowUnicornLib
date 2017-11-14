4018e88892e2d973b357700a99f74462ba893fdf
dhu92 <dhutter92@gmail.com>
2017-11-14 10:45:37 +0100

    Fix BorrowController

--- src/main/java/at/fhv/team3/application/BookingController.java
+++ src/main/java/at/fhv/team3/application/BookingController.java
@@ -5,14 +5,17 @@ import at.fhv.team3.domain.dto.DTO;
 import at.fhv.team3.persistence.BookingRepository;
 import at.fhv.team3.rmi.interfaces.RMIBooking;
 
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
 /**
  * Created by David on 11/14/2017.
  */
-public class BookingController implements RMIBooking{
+public class BookingController extends UnicastRemoteObject implements RMIBooking{
 
     private BookingRepository _bookingRepository;
 
-    public BookingController(){
+    public BookingController() throws RemoteException{
         _bookingRepository = BookingRepository.getInstance();
     }
 
