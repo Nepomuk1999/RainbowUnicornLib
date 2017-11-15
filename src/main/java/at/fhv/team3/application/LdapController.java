2676bd23c5e586163c9b5093d9dc31ef586ba31a
Nepomuk1999 <christoph.bauer@students.fhv.at>
2017-11-15 23:06:20 +0100

    ldap

---
+++ src/main/java/at/fhv/team3/application/LdapController.java
@@ -0,0 +1,35 @@
+package at.fhv.team3.application;
+
+import javax.naming.*;
+import java.util.Properties;
+
+public class LdapController {
+
+    private String url;
+    private Properties env;
+
+    public LdapController() {
+        url = "ldap://openldap.fhv.at";
+        env = new Properties();
+    }
+
+    public boolean authenticateUser(String name, String password) throws NamingException {
+        boolean acess = false;
+        env.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory");
+        env.put(Context.PROVIDER_URL, url);
+        env.put(Context.SECURITY_AUTHENTICATION, "simple");
+        env.put(Context.SECURITY_PRINCIPAL, "userid=username,ou=people,o=fhv.at");
+        env.put(Context.SECURITY_CREDENTIALS, password);
+
+        try {
+            Context ctx = new InitialContext(env);
+            ctx.close();
+            acess = true;
+
+        } catch (NamingException ex) {
+            acess = false;
+        }
+        return acess;
+    }
+
+}
