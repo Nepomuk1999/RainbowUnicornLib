fe3c7b673f2bd187c7fc97ce779a2c316abfb1a5
janflei <fleisch.ja@gmail.com>
2017-11-16 23:46:16 +0100

    KeyDTO für publickey EasyCrypt mit RSA

--- src/main/java/at/fhv/team3/rmi/interfaces/RMILdap.java
+++ src/main/java/at/fhv/team3/rmi/interfaces/RMILdap.java
@@ -1,10 +1,12 @@
 package at.fhv.team3.rmi.interfaces;
 
 import at.fhv.team3.domain.dto.EmployeeDTO;
+import at.fhv.team3.domain.dto.KeyDTO;
 
 import javax.naming.NamingException;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
+import java.security.PrivateKey;
 
 /**
  * Created by David on 11/15/2017.
@@ -12,5 +14,7 @@ import java.rmi.RemoteException;
 public interface RMILdap extends Remote {
 
     public EmployeeDTO authenticateUser(String name, String password) throws NamingException, RemoteException;
+
+    public KeyDTO getPublicKey() throws NamingException, RemoteException;
 
 }
